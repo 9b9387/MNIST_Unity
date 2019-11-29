35e58740c0656ba5349b6f4e1b99182aee562c8c
shihongyang <9b9387@gmail.com>
2019-11-29 12:57:30 +0800

    init

---
+++ Assets/Scripts/DoodleBoard.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DoodleBoard : MonoBehaviour, IDragHandler
+{
+    private RectTransform rectTransform;
+    private Texture2D texture2D;
+
+    private readonly int PAINTER_SIZE = 15;
+    private readonly int WIDTH = 560;
+    private readonly int HEIGHT = 560;
+    private readonly int GRID_SIZE = 20;
+
+    public bool showGrid = false;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        texture2D = new Texture2D(WIDTH, HEIGHT);
+        Clear();
+
+        var image = GetComponent<Image>();
+        image.sprite = Sprite.Create(texture2D, new Rect(0, 0, WIDTH, HEIGHT), Vector2.zero);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, null, out Vector2 localPoint);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position - eventData.delta, null, out Vector2 lastPoint);
+
+        for (int j = 0; j < 10; j++)
+        {
+            int textureX = (int)Mathf.Lerp(localPoint.x, lastPoint.x, j / 10f);
+            int textureY = (int)Mathf.Lerp(localPoint.y, lastPoint.y, j / 10f);
+
+            for (int x = textureX - PAINTER_SIZE; x < textureX + PAINTER_SIZE; x++)
+            {
+                for (int y = textureY - PAINTER_SIZE; y < textureY + PAINTER_SIZE; y++)
+                {
+                    var rect = rectTransform.rect;
+                    if (rect.Contains(new Vector2(x, y)) == false)
+                    {
+                        continue;
+                    }
+                    texture2D.SetPixel(x, y, Color.red);
+                }
+            }
+        }
+        texture2D.Apply();
+    }
+
+    public void Clear()
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                if (showGrid && (x % GRID_SIZE == 0 || y % GRID_SIZE == 0))
+                {
+                    texture2D.SetPixel(x, y, Color.blue);
+                }
+                else
+                {
+                    texture2D.SetPixel(x, y, Color.black);
+                }
+            }
+        }
+
+        texture2D.Apply();
+    }
+
+    public void PrintData()
+    {
+        // Be consistent with MNIST Data (28x28)
+        var col = WIDTH / GRID_SIZE;    // 28
+        var row = HEIGHT / GRID_SIZE;   // 28
+        var space = (float)GRID_SIZE;
+        var data = new float[col, row];
+
+        for (int x = 0; x < col; x++)
+        {
+            for (int y = 0; y < row; y++)
+            {
+                float sum = 0;
+                for (int i = (int)(x * space); i < (x * space) + space; i++)
+                {
+                    for (int j = (int)(y * space); j < (y * space) + space; j++)
+                    {
+                        sum += texture2D.GetPixel(i, j).r;
+                    }
+                }
+                // average
+                sum /= space * space;
+                // revert y axis
+                data[x, row - 1 - y] = sum;
+            }
+        }
+
+        var str = "[";
+        for (int i = 0; i < 28; i++)
+        {
+            str += "[";
+            for (int j = 0; j < 28; j++)
+            {
+                if (j != 0)
+                {
+                    str += ", ";
+                }
+                str += data[j, i];
+            }
+            str += "],\n";
+        }
+        str += "]";
+        Debug.Log(str);
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# MNIST Unity
+
+该项目用于手写数据生成，测试MNIST模型的准确性。
+
+## 生成数据
+
+运行Unity，在屏幕手写后，点击PrintData按钮
+![](./Document/48E3FB73-4D43-48EC-BDBC-003E6B7682B9.png)
+
+输出MNIST数据集格式的数据
+![](./Document/149D6505-4DC1-4F46-803E-1E347C5232C7.png)
+
+## 验证数据
+
+验证环境为jupyter，复制输出的数据到jupyter笔记中并显示图像。
+
+![](./Document/C1058B73-1AFD-4D41-B345-C0CE43BA8627.png)
+
+验证结果，红色柱状图显示 为6的概率最高
+
+![](./Document/0B38D2F0-E26F-418E-BB1D-CF33B09E6D94.png)
